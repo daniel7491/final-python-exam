4d2423af4ae70758cf348348735e48eadf347324
daniel7491 <dnai745@gmail.com>
2024-07-20 17:45:25 +0300

    adding part 3

---
+++ scripts/deploy_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/ubuntu/app
+docker pull daniel745/python-exam:latest
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker run -d -p 80:80 daniel745/python-exam:latest
