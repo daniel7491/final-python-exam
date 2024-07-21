a2ff7e01ca28115deb19c4dbab6cfa56c03b1c53
daniel7491 <107433451+daniel7491@users.noreply.github.com>
2024-07-21 21:45:48 +0300

    Update deploy_docker.sh

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/app
-docker pull daniel745/python-exam:latest
+docker pull daniel745/python-exam:1
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
-docker run -d -p 80:80 daniel745/python-exam:latest
+docker run -d -p 80:80 daniel745/python-exam:1
